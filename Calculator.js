c3466db80acb008194aa867365e9c885ef9792da
Jesbin Jobi <200588911@student.georgianc.on.ca>
2024-09-30 15:16:45 -0400

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,15 @@
+
+function calculator(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            return 'Error: Invalid operator';
+    }
+}
